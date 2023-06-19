744b08d5bcd3da081b532c8b4684abbca08212d0
Joacil <joacil.amarante@gmail.com>
2023-06-19 19:02:30 -0400

    19-06-23

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -8,13 +8,42 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
 
+
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 0;
+
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
+
+
+            filme.Id = id++;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filme.Duracao);
+            //ao criar o filme, retorna o filme que foi adicionado
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+            //Console.WriteLine(filme.Titulo);
+            //Console.WriteLine(filme.Duracao);
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+        {
+            return filmes.Skip(skip).Take(take);
+            //skip =  pular... a partir de x  
+            //take = pegar... pegue x registro
+            //tem como objetivo otimizar a consulta
+        }
+
+        //interrogação significa que ele pode retorna nulo
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            //para cada elementos de filmes, busco id do filme, igual id parametro, se for retorna o id, caso contrario return nul
+            var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null) return NotFound();
+            return Ok(filme);
+
         }
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,9 +1,21 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
+
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Título do filme é obrigatório")]
+        [StringLength(30)]
         public string Titulo { get; set; }
+        [Required]
         public string Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duração deve conter em 70 e 600 minutos!")]
         public int Duracao { get; set; }
+
     }
 }
